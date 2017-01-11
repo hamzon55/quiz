8dd858801955b261789eeb6aea1740ab93a1adb1
hamzon55 <hamzon.jb@gmail.com>
2017-01-11 03:31:19 +0100

    Create First Quiz

--- app/src/main/java/com/lasalle/quiz/MainActivity.java
+++ app/src/main/java/com/lasalle/quiz/MainActivity.java
@@ -1,7 +1,11 @@
 package com.lasalle.quiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+         Button b1 = (Button) findViewById(R.id.b1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, QuizOne.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/com/lasalle/quiz/QuizOne.java
@@ -0,0 +1,19 @@
+package com.lasalle.quiz;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class QuizOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quizone);
+
+
+            }
+
+    }
+
+
