f0fa226ea509031e27b20b24dba2c0cffe708ee1
hamzon55 <hamzon.jb@gmail.com>
2017-01-13 12:26:07 +0100

    create Question class for models

---
+++ app/src/main/java/com/lasalle/quiz/models/Answer.java
@@ -0,0 +1,7 @@
+package com.lasalle.quiz.models;
+
+/**
+ * Created by hamza on 13/01/2017.
+ */
+public class Answer {
+}
---
+++ app/src/main/java/com/lasalle/quiz/models/Question.java
@@ -0,0 +1,42 @@
+package com.lasalle.quiz.models;
+
+import java.util.List;
+
+/**
+ * Created by hamza on 13/01/2017.
+ */
+
+public class Question {
+
+    private String text ;
+    private List<Answer> answerList;
+
+
+
+    public Question(String text, List<Answer> answerList) {
+        this.text = text;
+        this.answerList = answerList;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public List<Answer> getAnswerList() {
+        return answerList;
+    }
+
+    public void setAnswerList(List<Answer> answerList) {
+        this.answerList = answerList;
+    }
+
+
+
+
+
+
+}
