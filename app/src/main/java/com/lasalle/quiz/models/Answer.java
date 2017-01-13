ea6450cb988d5caad06a394d0acec63713e31c13
hamzon55 <hamzon.jb@gmail.com>
2017-01-13 12:28:33 +0100

    add Answer class for models

--- app/src/main/java/com/lasalle/quiz/models/Answer.java
+++ app/src/main/java/com/lasalle/quiz/models/Answer.java
@@ -4,4 +4,33 @@ package com.lasalle.quiz.models;
  * Created by hamza on 13/01/2017.
  */
 public class Answer {
+
+
+    private  String text;
+    private  boolean correct ;
+
+    public Answer(String text, boolean correct) {
+        this.text = text;
+        this.correct = correct;
+    }
+    
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public boolean isCorrect() {
+        return correct;
+    }
+
+    public void setCorrect(boolean correct) {
+        this.correct = correct;
+    }
+
+
+
+
 }
